476c0de5174b805d35ab380bb24e01aff3b968fb
thoang3 <thai-son.hoang@universite-paris-saclay.fr>
2025-06-27 00:57:22 +0200

    Update database and file document

--- scripts/auth-signin.js
+++ scripts/auth-signin.js
@@ -1,7 +1,7 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { app } from "/scripts/firebase-config.js"; // Import app from your firebase-config.js
 
-const auth = getAuth(app);
+export const auth = getAuth(app);
 
 export function mySignInWithEmailAndPassword(email, password) {
   return signInWithEmailAndPassword(auth, email, password)
--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -6,7 +6,7 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
     apiKey: "AIzaSyD841HMHmtZf90-WJruEM5Jre4WagLOyiY",
     authDomain: "mon-amour-manage.firebaseapp.com",
-    projectId: "mon-amour-manage",
+    projectId: "mon-amour-manage-5d822",
     storageBucket: "mon-amour-manage.firebasestorage.app",
     messagingSenderId: "511777334853",
     appId: "1:511777334853:web:fbe373f3376828716b252c",
