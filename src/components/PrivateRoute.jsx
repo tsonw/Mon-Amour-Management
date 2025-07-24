2537f1915e586021ae4fa8a56447d8ee76f8a292
thoang3 <thai-son.hoang@universite-paris-saclay.fr>
2025-07-24 02:11:34 +0200

    Update Loading interface

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,10 +2,24 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../scripts/onAuthStateChanged";
 
+import "../styles/components-styles/loading.css";
+
+import logo from "../assets/iconMA.png"
+
 function PrivateRoute({ children }) {
     const { user, loading } = useAuth();
     
-    if (loading) return <div>Loading...</div>;
+    if (loading) {
+        return (
+            <>
+                <div className="main-loading">
+                    <div className="loading-box">
+                        <img className="loading-img" src={logo} alt="icon" draggable="false" />
+                    </div>
+                </div>
+            </>
+        );
+    }
 
     // Kiểm tra user đã đăng nhập hay chưa nếu chưa thì đưa về trang Login
     if (!user) return <Navigate to="/" replace />;
